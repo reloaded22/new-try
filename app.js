d303043bc91c4318c0e9c12c039bc83fdfff94d6
reloaded22 <andres.rojas.talent@gmail.com>
2022-12-11 13:47:28 -0500

    Add '/api' to the root route

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 //////////////////////////////////////
 
 // Use the routes //
-app.use("/", userRouter);
+app.use("/api", userRouter);
 
 // SERVER CONNECTION //
 const PORT = process.env.PORT || 3000;
